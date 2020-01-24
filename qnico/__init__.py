6b8bbdb38222df110a29dab9134e950db38da28a
sakura067m <3IE19001M@s.kyushu-u.ac.jp>
2020-01-24 22:28:10 +0900

    v2.2

--- qnico/__init__.py
+++ qnico/__init__.py
@@ -1,3 +1,4 @@
+import sys
 from .getin import lk
 
 # init variable
@@ -7,8 +8,10 @@ config = ConfigParser()
 config.read_string(pkgutil.get_data(__package__, "config/data.cfg").decode())
 
 import logging
+handler = logging.StreamHandler(sys.stdout)
 base_logger = logging.getLogger(__package__)
 base_logger.setLevel(config["USER"]["loglevel"])
+base_logger.addHandler(handler)
 
 # start loading
 from .worker import NicoJob
